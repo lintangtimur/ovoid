225a3bd502bc73a6f7ebec85619ded76841bf744
lintangtimur <lintangtimur915@gmail.com>
2018-12-17 02:04:34 +0700

    tests

---
+++ tests/OvoidTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Stelin\OVOID;
+use Stelin\Meta\Meta;
+
+class OvoidTest extends TestCase
+{
+    /**
+     * ovoid
+     *
+     * @var \Stelin\OVOID
+     */
+    private $ovoid;
+
+    /**
+     * curl
+     *
+     * @var \Stelin\HTTP\Curl
+     */
+    private $curl;
+
+    public function setUp()
+    {
+        $this->ovoid = new OVOID();
+    }
+
+    public function tearDown()
+    {
+    }
+
+    public function testMetaValue()
+    {
+        $this->assertSame('C7UMRSMFRZ46D9GW9IK7', Meta::APP_ID);
+        $this->assertNotSame('asd', META::APP_ID);
+        $this->assertIsString(Meta::APP_ID);
+
+        $this->assertSame('2.5.0', Meta::APP_VERSION);
+        $this->assertNotSame('1', Meta::APP_VERSION);
+        $this->assertSame('Android', Meta::OS_NAME);
+        $this->assertSame('8.1.0', Meta::OS_VERSION);
+        $this->assertSame('02:00:00:44:55:66', Meta::MAC_ADDRESS);
+        $this->assertSame('5d1fa7f9-fd99-3bae-95d5-67fedb901502', Meta::DEVICE_ID);
+    }
+
+    public function testEndpointValue()
+    {
+        $this->assertSame('https://api.ovo.id/', OVOID::BASE_ENDPOINT);
+        $this->assertNotEquals('https://api.ovo.id', OVOID::BASE_ENDPOINT);
+    }
+}
