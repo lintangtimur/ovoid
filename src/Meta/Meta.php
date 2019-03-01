c8a6a01be813a72b03cc15fdb50098027bfac827
lintangtimur <lintangtimur915@gmail.com>
2019-03-01 21:27:47 +0700

    update app version to 2.8.0

--- src/Meta/Meta.php
+++ src/Meta/Meta.php
@@ -10,7 +10,7 @@ namespace Stelin\Meta;
 class Meta
 {
     const APP_ID = 'C7UMRSMFRZ46D9GW9IK7';
-    const APP_VERSION = '2.5.0';
+    const APP_VERSION = '2.8.0';
     const OS_NAME = 'Android';
     const OS_VERSION = '8.1.0';
     const MAC_ADDRESS = '02:00:00:44:55:66';
